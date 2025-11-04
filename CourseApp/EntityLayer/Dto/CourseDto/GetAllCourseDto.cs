0ee704c2adc96659722815b2cd038d09c84ebef0
oguzhan0170 <oguzhannyavuzz17@gmail.com>
2025-11-04 23:47:18 +0300

    fix(CourseManager): GetAllAsync metodunda N+1 sorgu problemi giderilmesi için Include(c => c.Instructor) eklendi ve InstructorName alanı tanımlandı.

--- CourseApp/EntityLayer/Dto/CourseDto/GetAllCourseDto.cs
+++ CourseApp/EntityLayer/Dto/CourseDto/GetAllCourseDto.cs
@@ -9,4 +9,7 @@ public class GetAllCourseDto
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
     public string? InstructorID { get; set; }
+    // tek sorguada çekilmesi için 
+    public string? InstructorName { get; set; }
+
 }
